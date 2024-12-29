3e86a76c1b6488e35ac2da4c4355b8a2a33a484f
u-nel <no.elsaesser@googlemail.com>
2024-12-29 12:44:43 +0100

    Initial commit

---
+++ src/IBaseEntity.php
@@ -0,0 +1,11 @@
+<?php
+namespace bg13\entites;
+
+
+/**
+ * All entity classes have to implement this basic interface, providing the
+ * functionality to tell about the class, the name, errors, ...
+ */
+interface IBaseEntity {
+    
+}
